496fb437edd184cd945d4e72f0e552819ac09dd9
Hank Hester <hankhester3@gmail.com>
2021-06-02 16:43:56 -0700

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,33 @@
-// #[macro_use]
-// extern crate diesel;
-// #[macro_use]
-// extern crate log;
-// #[macro_use]
-// extern crate serde_derive;
-// #[macro_use]
-// extern crate tera;
-
 use std::{env, io};
 
-// use actix_files as fs;
-// use actix_session::CookieSession;
-use actix_web::middleware::{errhandlers::ErrorHandlers, Logger};
-use actix_web::{http, web, App, HttpServer};
-// use dotenv::dotenv;
-// use tera::Tera;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
 
 mod api;
-// mod db;
-// mod model;
-// mod schema;
-// mod session;
-
-// static SESSION_SIGNING_KEY: &[u8] = &[0; 32];
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    // dotenv().ok();
-
     env::set_var("RUST_LOG", "actix_todo=debug,actix_web=info");
-    // env_logger::init();
 
-    // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    // let pool = db::init_pool(&database_url).expect("Failed to create pool");
+    // let app = move || {
+    //     println!("Constructing the App");
 
-    let app = move || {
-        // debug!("Constructing the App");
-        println!("Constructing the App");
+    //     App::new()
+    //         .wrap(Logger::default())
+    //         .route("/", web::get().to(api::index))
+    // };
 
-        // let templates: Tera = compile_templates!("templates/**/*");
+    // println!("Starting server");
+    // HttpServer::new(app).bind("0.0.0.0:8088")?.run().await
 
-        // let session_store = CookieSession::signed(SESSION_SIGNING_KEY).secure(false);
-
-        // let error_handlers = ErrorHandlers::new()
-        //     .handler(
-        //         http::StatusCode::INTERNAL_SERVER_ERROR,
-        //         api::internal_server_error,
-        //     )
-        //     .handler(http::StatusCode::BAD_REQUEST, api::bad_request)
-        //     .handler(http::StatusCode::NOT_FOUND, api::not_found);
-
-        App::new()
-            // .data(templates)
-            // .data(pool.clone())
-            .wrap(Logger::default())
-            // .wrap(session_store)
-            // .wrap(error_handlers)
-            .route("/", web::get().to(api::index))
-        // .service(web::resource("/todo").route(web::post().to_async(api::create)))
-        // .service(
-        //     web::resource("/todo/{id}").route(web::post().to_async(api::update)),
-        // )
-        // .service(fs::Files::new("/static", "static/"))
-    };
-
-    // debug!("Starting server");
     println!("Starting server");
-    HttpServer::new(app).bind("0.0.0.0:8088")?.run().await
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .route("/", web::get().to(api::index))
+    })
+    .bind("0.0.0.0:8088")?
+    .run()
+    .await
 }
